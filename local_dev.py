2c9147d0c9ad632c251aea1285072c662e3c71b1
chrisrogers37 <christophertrogers37@gmail.com>
2025-02-18 21:32:42 -0500

    Update docker ports and files

--- local_dev.py
+++ local_dev.py
@@ -11,5 +11,5 @@ os.environ['FLASK_ENV'] = 'development'
 from app import application
 
 if __name__ == '__main__':
-    # Run in debug mode on port 8080
-    application.run(host='0.0.0.0', port=8080, debug=True) 
+    # Run in debug mode on port 8000
+    application.run(host='0.0.0.0', port=8000, debug=True) 
