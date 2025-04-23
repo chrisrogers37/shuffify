25d9aaaaa761a9a2ab95e086d674eae7c90a7059
chrisrogers37 <christophertrogers37@gmail.com>
2025-04-23 14:15:10 -0400

    Updates for consolidation

--- app/utils/shuffle_algorithms/__init__.py
+++ app/utils/shuffle_algorithms/__init__.py
@@ -1,58 +1,38 @@
-from abc import ABC, abstractmethod
-from typing import List, Optional, Dict
-from spotipy import Spotify
-from app.services.track_cache import TrackCache
+from typing import List, Optional, Dict, Any, Protocol
+from app.spotify.client import SpotifyClient
+import random
+import logging
 
-class ShuffleAlgorithm(ABC):
-    """Base class for shuffle algorithms."""
+logger = logging.getLogger(__name__)
+
+class ShuffleAlgorithm(Protocol):
+    """Interface for shuffle algorithms."""
     
-    @abstractmethod
-    def shuffle(self, tracks: List[str], sp: Optional[Spotify] = None, **kwargs) -> List[str]:
+    @property
+    def name(self) -> str:
+        """Name of the algorithm."""
+        ...
+    
+    @property
+    def description(self) -> str:
+        """Description of what the algorithm does."""
+        ...
+    
+    @property
+    def parameters(self) -> dict:
+        """Parameters that can be configured for this algorithm."""
+        ...
+    
+    def shuffle(self, tracks: List[str], sp: Optional[SpotifyClient] = None, **kwargs) -> List[str]:
         """
-        Shuffle the given tracks using the algorithm's strategy.
+        Shuffle the tracks according to the algorithm.
         
         Args:
             tracks: List of track URIs to shuffle
-            sp: Optional Spotify client for algorithms that need track metadata
-            **kwargs: Additional algorithm-specific parameters
+            sp: SpotifyClient for fetching audio features
+            **kwargs: Additional parameters specific to the algorithm
             
         Returns:
-            Shuffled list of track URIs
+            List of shuffled track URIs
         """
-        pass
-    
-    @property
-    @abstractmethod
-    def name(self) -> str:
-        """Human-readable name of the algorithm."""
-        pass
-    
-    @property
-    @abstractmethod
-    def description(self) -> str:
-        """Description of how the algorithm works."""
-        pass
-    
-    @property
-    @abstractmethod
-    def parameters(self) -> dict:
-        """
-        Dictionary describing the algorithm's parameters.
-        Example:
-        {
-            'keep_first': {
-                'type': 'integer',
-                'description': 'Number of tracks to keep at start',
-                'default': 0,
-                'min': 0
-            }
-        }
-        """
-        pass
-    
-    def get_track_features(self, sp: Spotify, tracks: List[str]) -> Dict[str, Dict[str, float]]:
-        """
-        Get audio features for tracks, using cache when possible.
-        This is available to all algorithms but only used by those that need it.
-        """
-        return TrackCache.load_track_features(sp, tracks) 
+        ... 
